a80dbf33a8e10d999ca924dea8c9bdc0cd6cc747
beomlee <lhbhb21c@gmail.com>
2023-12-27 23:42:09 +0900

    ccc

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var app = express();
+var server = express();
 
-app.get('/cities.json', function(req, res){
+server.get('/cities.json', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({insecticons : ["San Francisco","Amsterdam", "Berlin", "New York"]}));
     res.end();
 });
 
-app.get('/gukwha', function(req, res){
+server.get('/gukwha', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({insecticons : ["으악!!!!!!!!!!!!!!!!"]}));
     res.end();
 });
 
 var port = process.env.PORT || 4000;
-app.listen(port);
+server.listen(port);
 
-module.exports = app;
+module.exports = server;
